ee4b1cfc715b8a8915b8404d0b4223e11ff466af
GhostZero29 <jdpaterm2002@gmail.com>
2024-10-04 16:52:17 -0500

    Upgrade_2

--- LibraryApp/src/main/java/com/mycompany/libraryapp/Material.java
+++ LibraryApp/src/main/java/com/mycompany/libraryapp/Material.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.libraryapp;
 
 /**
@@ -11,66 +10,109 @@ public class Material {
     private String dateRecord;
     private int registerQuantity;
     private int actualAmount;
+    private int renewCount;
 
-
-    public Material(String identifier, String title, String dateRecord) {
+    public Material(String identifier, String title, String dateRecord, int registerQuantity) {
         this.identifier = identifier;
         this.title = title;
         this.dateRecord = dateRecord;
         this.registerQuantity = registerQuantity;
         this.actualAmount = registerQuantity;
-    }   
+        this.renewCount = 0;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDateRecord() {
+        return dateRecord;
+    }
+
+    public void setDateRecord(String dateRecord) {
+        this.dateRecord = dateRecord;
+    }
+
+    public int getRegisterQuantity() {
+        return registerQuantity;
+    }
+
+    public void setRegisterQuantity(int registerQuantity) {
+        this.registerQuantity = registerQuantity;
+    }
+
+    public int getActualAmount() {
+        return actualAmount;
+    }
+
+    public void setActualAmount(int actualAmount) {
+        this.actualAmount = actualAmount;
+    }
     
-    public void register(String identifier, String title, String dateRecord) {
-        if(identifier == null || identifier.isEmpty()){
-            System.out.println("The material identifier cannot be empty.");
-            return;
+    public void update(String title, String dateRecord){
+        this.title = title;
+        this.dateRecord= dateRecord;
+        System.out.println("Correctly updated material.");
+    }
+    public void increaseRegisteredQuantity(int additionalUnits){
+        if (additionalUnits > 0){
+            this.registerQuantity += additionalUnits;
+            this.actualAmount += additionalUnits;
+            System.out.println("Registered amount increased by"+ additionalUnits + "units" );
         }
-         this.identifier = identifier;
-         this.title = title;
-         this.dateRecord = dateRecord;
-         this.actualAmount = this.registerQuantity;
-     }
-    public void delete(String identifier) {
-        if (this.identifier.equals(identifier)) {
-            System.out.println("The material identifier does not match.");
-            return;
-            }
-            // Falta Logica para borrado
-            this.identifier = null;
-            this.title = null;
-            this.dateRecord = null;
+        else{
+            System.out.println("The amount to be increased must be greater than zero");
         }
-    
-    public void update(String identifier, String title, String dateRecord) {
-        if (this.identifier.equals(identifier)) {
-            this.title = title;
-            this.dateRecord = dateRecord;
+        
+    }
+    public void makeLoan(){
+        if (this.actualAmount > 0){
+            this.actualAmount--;
+            System.out.println("Registered loan for the material:"+ this.title);
+        }
+        else{
+            System.out.println("No units available for loan of the material: "+ this.title);
         }
     }
-
-    public void increaseRegisteredQuantity(String identifier) {
-        if (this.identifier.equals(identifier)) {
-            this.registerQuantity++;  // Incrementador
+    public void renew(){
+        if (this.renewCount < 3){
+            this.renewCount++;
+            System.out.println("Renewed loan for the material:"+ this.title);
+        }
+        else{
+            System.out.println("The loan of the material cannot be renewed more than 3 times:" + this.title);
         }
     }
-
-    public void makeLoan(String identifier, String idCard) {
-        if (this.identifier.equals(identifier) && this.actualAmount > 0) {
-            this.actualAmount--;  // Dimuye cantidad dispo
+    public void returnMaterial(){
+        if(this.actualAmount < this.registerQuantity){
+            this.actualAmount++;
+            System.out.println("Return registered for the material:" + this.title);
+        }
+        else{
+            System.out.println("All copies of this material are already in the library." + this.title);
         }
     }
-
-    public void renew(String identifier, String idCard) {
-        if (this.identifier.equals(identifier)) {
-            // Falta Logica para renovar Prestamos
-        }
-    }
-
-    public void returnMaterial(String identifier, String idCard) {
-        if (this.identifier.equals(identifier)) {
-            this.actualAmount++;  // Aumenta la cantiadad al devolver el libre,etc.
-        }
+    @Override
+    public String toString(){
+        return "Material{" +
+                "Identifier='" + identifier + '\'' +
+                ", Title='" + title + '\'' +
+                ", dateRecord='" + dateRecord + '\'' +
+                ", registerQuantity=" + registerQuantity +
+                ", actualAmount=" + actualAmount +
+                ", renewCount=" + renewCount +
+                '}';
     }
 }
-
